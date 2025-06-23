ff8675940c1df82a81be27e27d7c82391e52a6fc
adarshswaminath <adarshswaminath7@gmail.com>
2025-06-23 10:01:41 +0530

    feat: use the timestamps as true to create the timestamp for the Notes

--- src/models/Notes.model.ts
+++ src/models/Notes.model.ts
@@ -3,18 +3,16 @@ import type { NoteAttribute } from "../types/notes.js";
 import { sequelizeInstance } from "../config/db.js";
 
 export class Note extends Model<NoteAttribute> implements NoteAttribute {
-    declare id?: number;
-    declare title: string;
-    declare content: string;
-    declare createdAt?: Date;
-    declare updatedAt?: Date;
+  declare id?: number;
+  declare title: string;
+  declare content: string;
 }
 
-
-Note.init({
-    id: {type: DataTypes.INTEGER,primaryKey: true,autoIncrement: true},
-    title: {type: DataTypes.STRING,allowNull: false},
-    content: {type: DataTypes.STRING,allowNull: false},
-    createdAt: {type: DataTypes.DATE,allowNull: false},
-    updatedAt: {type: DataTypes.DATE,allowNull: false}
-},{sequelize: sequelizeInstance,modelName: "Notes",timestamps: true})
+Note.init(
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    content: { type: DataTypes.STRING, allowNull: false },
+  },
+  { sequelize: sequelizeInstance, modelName: "Notes", timestamps: true }
+);
--- src/types/notes.ts
+++ src/types/notes.ts
@@ -2,6 +2,4 @@ export type NoteAttribute ={
     id?: number;
     title: string;
     content: string;
-    createdAt?: Date;
-    updatedAt?: Date;
 }
